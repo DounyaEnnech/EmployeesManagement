37f6976a7b4c1ce2f5882b3988c40fdd8feb159d
ENNECH DOUNYA <32358944+DounyaEnnech@users.noreply.github.com>
2020-02-14 14:02:14 +0100

    Update employeeC.php

--- uha_archiweb_2020/controllers/employeeC.php
+++ uha_archiweb_2020/controllers/employeeC.php
@@ -1,88 +1,35 @@
 <?php
-require MODELS.DS.'employeeM.php';
-
-require CLASSES.DS.'view.php';
 class EmployeeController {
+    public function construct(){}
 
-    public function construct(){
-
+    public function index() {
+      $this->list();
+    }
+    public function list(){
+      require_once MODELS.DS.'employeeM.php';
+      $m=New EmployeeModel();
+      $employees=$m->listAll();
+      // Affichage au sein de la vue des données récupérées via le model
+      require_once CLASSES.DS.'view.php';
+      $v=new View();
+      $v->setVar('employeelist',$employees);
+      $v->render('employee','list');
+    }
+    public function view($id=null){
+      require_once MODELS.DS.'employeeM.php';
+      $m=New EmployeeModel();
+      require_once CLASSES.DS.'view.php';
+      $v=new View();
+      if ($employee=$m->listOne($id)) $v->setVar('e',$employee);
+      // Affichage au sein de la vue des données récupérées via le model
+      $v->render('employee','view');
+    }
+    public function edit($id=null){
+      die('modification d\'un employé');
+    }
+    public function delete($id=null){
+      die('suppression d\'un employé');
     }
 
-    public function liste(){
-        $model=new EmployeeModel();
-        $lesEmployees=$model->liste();
-        $v=new View();
-        $v->render('employesList',$lesEmployees);
-    //   $this->renderview('employesList',$lesEmployees);
-
-    }
-
-    public function listOne($id)
-    {
-
-        $model = new EmployeeModel();
-        $Employee=$model->listOne($id);
-        //$data=$Employee->fetch(PDO::FETCH_OBJ);
-        //return $Employee;
-        $v=new View();
-
-        $v->render('listOne',$Employee);
-    }
-    public function addEmployee()
-    {
-
-        $this->renderview('addEmployee','');
-    }
-    public function EditEmploye($id)
-    {
-        $model = new EmployeeModel();
-        $Employee=$model->listOne($id);
-        $v=new View();
-        $v->render('editEmployee',$Employee);
-
-    }
-
-    public function UpdateEmployee()
-    {
-        $model = new EmployeeModel();
-        if(isset( $_POST['EmployeeId'])) {
-            $EmployeeId = $_POST['EmployeeId'];
-            $NationalIDNumber = $_POST['NationalIDNumber'];
-            $ContactID = $_POST['ContactID'];
-            $LoginID = $_POST['LoginID'];
-            $Title = $_POST['Title'];
-
-            $model->UpdateEmployee($EmployeeId, $NationalIDNumber, $ContactID, $LoginID, $Title);
-        }
-
-    }
-
-    public function InsertEmployee()
-    {
-        $EmployeeId = $_POST['EmployeeId'];
-        $NationalIDNumber = $_POST['NationalIDNumber'];
-        $ContactID = $_POST['ContactID'];
-        $LoginID = $_POST['LoginID'];
-        $Title = $_POST['Title'];
-        $BirthDate = $_POST['BirthDate'];
-        $MaritalStatus = $_POST['MaritalStatus'];
-        $Gender = $_POST['Gender'];
-        $HireDate = $_POST['HireDate'];
-        $SalariedFlag = ($_POST['SalariedFlag']);
-        $VacationHours = $_POST['VacationHours'];
-        $SickLeaveHours = $_POST['SickLeaveHours'];
-        $CurrentFlag = ($_POST['CurrentFlag']);
-        $rowguid = $_POST['rowguid'];
-        $ModifiedDate = $_POST['ModifiedDate'];
-        $model=new EmployeeModel();
-        $model->NewEmployee($EmployeeId,$NationalIDNumber,$ContactID,$LoginID,$Title,$BirthDate,$MaritalStatus,$Gender,$HireDate,$SalariedFlag,$VacationHours,$SickLeaveHours,$CurrentFlag,$rowguid,$ModifiedDate);
-
-    }
-    public function deleteEmployee($id)
-    {
-        $model = new EmployeeModel();
-        $model->deleteEmployee($id);
-        $this->liste();
-    }
 }
 ?>
